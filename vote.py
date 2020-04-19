de461f72b56d0283c8e0a8ece7574e514aa63bac
dhilberg <37307908+dhilberg@users.noreply.github.com>
2020-04-18 18:17:50 -0700

    Added initial working script body.

---
+++ vote.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import os
+import argparse
+
+vote_url = 'http://www.artsonia.com/aotw/vote/fan-direct/84806714'
+
+log_directory = os.path.join(os.getcwd(), 'logs')
+log_path = os.path.join(log_directory, 'geckodriver.log')
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+geckodriver_path = 'C:\\Users\\Dennis\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe'
+
+browser = webdriver.Firefox(executable_path=geckodriver_path, service_log_path=log_path)
+
+count = 1
+max = 6
+while count < max:
+    print(f'Vote attempt {count} of {max - 1}')
+
+    browser.delete_all_cookies()
+    browser.get(vote_url)
+    browser.find_element_by_id('BtnVote_84806714').click()
+    count += 1
+
+browser.quit()
